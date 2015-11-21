77b2c945e32cf3306e6dc1b5b8e362f7c0b8e564
nik <nikit.cpp@yandex.ru>
2015-11-22 01:55:58 +0300

    react

---
+++ src/main/webapp/scripts/app.jsx
@@ -0,0 +1,104 @@
+void function() { 'use strict'
+
+var {DefaultRoute, Route, RouteHandler} = ReactRouter
+
+var NavItem = createActiveRouteComponent('li')
+
+var ActivePara = createActiveRouteComponent('p', {link: false})
+
+var App = React.createClass({
+  render() {
+    return <div className="App container">
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="container">
+          <span className="navbar-brand">Дикий залёт</span>
+          <ul className="nav navbar-nav">
+            <NavItem to="dashboard">Главная</NavItem>
+            <NavItem to="tasks">Почта</NavItem>
+            <li>
+              <p className="navbar-text"><span className="glyphicon glyphicon-user"></span>Вся мощь Wildfly</p>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <RouteHandler/>
+      <hr/>
+      <footer>
+        Подвал
+      </footer>
+    </div>
+  }
+})
+
+var Dashboard = React.createClass({
+  render() {
+    return <div className="Dashboard">
+      <h2>Dashboard</h2>
+      <p>I'm a regular paragraph.</p>
+      <ActivePara to="dashboard" activeClassName="special">
+        I get highlighted because the Dashboard route is active.
+      </ActivePara>
+    </div>
+  }
+})
+
+var Tasks = React.createClass({
+  render() {
+    return <div className="Tasks row">
+      <div className="col-md-3">
+        <ul className="nav nav-pills nav-stacked">
+          <NavItem to="e-mail">Почта</NavItem>
+          <NavItem to="hibernate">Hibernate</NavItem>
+          <li>
+            <p className="navbar-text"><code>&lt;li&gt;</code>s in this nav get the <code>active</code> class</p>
+          </li>
+        </ul>
+      </div>
+      <div className="col-md-9">
+        <ActivePara to="hibernate" activeClassName="special">
+          I get highlighted when the My Tasks route is active.
+        </ActivePara>
+        <RouteHandler/>
+      </div>
+    </div>
+  }
+})
+
+var TasksDashboard = React.createClass({
+  render() {
+    return <div className="TasksDashboard">
+      <h2>Tasks Dashboard</h2>
+    </div>
+  }
+})
+
+var Email = React.createClass({
+  render() {
+    return <div className="Email">
+      <h2>Почта</h2>
+    </div>
+  }
+})
+
+var MyTasks = React.createClass({
+  render() {
+    return <div className="MyTasks">
+      <h2>Hibernate</h2>
+    </div>
+  }
+})
+
+var routes = <Route handler={App}>
+  <DefaultRoute name="dashboard" handler={Dashboard}/>
+  <Route name="tasks" handler={Tasks}>
+    <DefaultRoute handler={TasksDashboard}/>
+    <Route name="e-mail" path="e-mail" handler={Email}/>
+    <Route name="hibernate" path="hibernate" handler={MyTasks}/>
+  </Route>
+</Route>
+
+ReactRouter.run(routes, function(Handler) {
+  React.render(<Handler/>, document.getElementById('content'))
+})
+
+}()
